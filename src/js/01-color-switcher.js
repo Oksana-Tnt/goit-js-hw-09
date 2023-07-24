3aef9839c6225aa458fab21b689a407320c82dea
Oksana-Tnt <127055817+Oksana-Tnt@users.noreply.github.com>
2023-07-24 17:53:36 +0200

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const refs = {
+    startEl : document.querySelector("[data-start]"),
+    stopEl : document.querySelector("[data-stop]"),
+    bodyEl : document.querySelector("body"),
+};
 
+let timerId = null;
+
+refs.startEl.addEventListener("click", onClickStart);
+refs.stopEl.addEventListener("click", onClickStop);
+
+
+function onClickStart(){
+
+    timerId = setInterval(() => {   
+
+        refs.bodyEl.style.backgroundColor=getRandomHexColor();  
+
+        refs.startEl.setAttribute("disabled", "disabled");
+      }, 1000);
+}
+
+function onClickStop(){
+
+    clearInterval(timerId);
+
+    refs.startEl.removeAttribute("disabled");
+}
+
+function getRandomHexColor() {
+
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+
+  }
---
+++ src/js/02-timer.js
@@ -0,0 +1,15 @@
+import flatpickr from "flatpickr";
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },
+  };
+
+ const flatpickr= new flatpickr("datetime-picker", options);
