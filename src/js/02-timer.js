97b45dd7d787fedafe9b67abe6e3f8d72a52a701
Oksana-Tnt <oksanatantsiura@gmail.com>
2023-07-25 16:31:16 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,54 +3,85 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
 const refs = {
+    inputEl : document.querySelector("#datetime-picker"),
     startEl : document.querySelector("[data-start]"),
     daysEl : document.querySelector("[data-days]"),
     hoursEl : document.querySelector("[data-hours]"),
     minutesEl : document.querySelector("[data-minutes]"),
     secondsEl : document.querySelector("[data-seconds]"),
 };
+
+refs.startEl.addEventListener("click", onClickStart);
         
     const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
-    onClose(selectedDates) {        
-     if((selectedDates[0]).getTime() < Date.now()){
-        alert("error");
-        refs.startEl.setAttribute("disabled", "disabled");
-        return;
-     }
-
-    refs.startEl.removeAttribute("disabled");       
-    let countDownDate = selectedDates[0];
-
-    refs.startEl.addEventListener("click", ()=>{
-        refs.startEl.setAttribute("disabled", "disabled");
-        const intervalId = setInterval(() => {
-            const currentTime = Date.now();
-            const distance = countDownDate - currentTime;
-            const {days, hours, minutes, seconds}=convertMs(distance);
-            refs.daysEl.textContent=days;
-            refs.hoursEl.textContent=hours;
-            refs.minutesEl.textContent=minutes;
-            refs.secondsEl.textContent=seconds;
-         if (distance < 0) {            
-            alert("EXPIRED");
-            clearInterval(intervalId);
-            refs.startEl.removeAttribute("disabled");
-            refs.daysEl.textContent=0;
-            refs.hoursEl.textContent=0;
-            refs.minutesEl.textContent=0;
-            refs.secondsEl.textContent=0;          
-         }
-      
-        }, 1000);     
-    });   
- 
-  },
+        onClose(selectedDates) {  
+           
+        if((selectedDates[0]).getTime() < Date.now()){
+            alert("error");
+            makeDisableButton();
+             return;
+        }
+            makeAbleButton();                  
+    },
 }; 
-  
+
+flatpickr("#datetime-picker", options);
+
+function getDate() {
+    let date = refs.inputEl._flatpickr.selectedDates[0].getTime();
+    return date;
+}
+
+function calcDistance(value) {     
+    const currentTime = Date.now();
+    const distance = value - currentTime;
+    return distance;
+}
+
+function fillCountDown(value) {
+    const { days, hours, minutes, seconds } = convertMs(value);
+    refs.daysEl.textContent=days;
+    refs.hoursEl.textContent=hours;
+    refs.minutesEl.textContent=minutes;
+    refs.secondsEl.textContent=seconds;
+}; 
+
+function makeAbleButton() {
+    refs.startEl.removeAttribute("disabled");
+}
+
+function makeDisableButton() {
+    refs.startEl.setAttribute("disabled", "disabled");
+}
+
+function onClickStart() {    
+    
+    makeDisableButton();
+
+    const intervalId = setInterval(() => {        
+    
+    const distance = calcDistance(getDate());
+        
+    fillCountDown(distance);
+        
+        if (distance < 0) {  
+            
+            alert("EXPIRED");
+            
+            clearInterval(intervalId);
+            
+            makeAbleButton();
+            
+            fillCountDown(0);        
+        }
+      
+    }, 1000);
+};   
+   
 function convertMs(ms) {
     // Number of milliseconds per unit of time
     const second = 1000;
@@ -70,4 +101,4 @@ function convertMs(ms) {
     return { days, hours, minutes, seconds };
   }
 
-  flatpickr("#datetime-picker", options);
+
